02390f40d64b71ac96819690a53321b83d02f59e
vagrant <vagrant@localhost.localdomain>
2016-05-17 23:40:16 +0900

    mergeして

--- server_side_laravel/lesson_todo/resources/views/todo/index.blade.php
+++ server_side_laravel/lesson_todo/resources/views/todo/index.blade.php
@@ -57,7 +57,7 @@
     </tbody>
 </table>
 </div>
-<div>merge ok</div>
+<div>merge okです</div>
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
