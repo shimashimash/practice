62f868c9864cc3de22db8caa76df0113af53d91b
vagrant <vagrant@localhost.localdomain>
2016-05-17 23:21:38 +0900

    hello world 削除

--- server_side_laravel/lesson_todo/resources/views/todo/index.blade.php
+++ server_side_laravel/lesson_todo/resources/views/todo/index.blade.php
@@ -57,9 +57,6 @@
     </tbody>
 </table>
 </div>
-<div>
-  <p style="float:right";>hello world</p>
-</div>
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
